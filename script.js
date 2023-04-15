e1a4df0bcb7f555fa95ef54187299b8a508edde0
Jonatas Oliveira <Jonatasoliveira@exemple.com>
2023-04-15 16:49:32 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+/*Botão hamburguer*/
+    const hamburguer = document.querySelector(".hamburguer");
+    const navMenu = document.querySelector(".nav-menu");
+
+    hamburguer.addEventListener("click", () => {
+    hamburguer.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  });
+  
+/**/
