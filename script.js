bb363a2d829758b6a8836d142cc9917c76e80012
Jônatas Oliveira <115200493+Jonatas-coding@users.noreply.github.com>
2023-09-23 00:04:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+/*Botão hamburguer*/
+    const hamburguer = document.querySelector(".hamburguer");
+    const navMenu = document.querySelector(".nav-menu");
+
+    hamburguer.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+  });
+
+  navMenu.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+  });
+  
